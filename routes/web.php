68b969e855ec0be75b2d412fbd535ba45c4f2280
hamedefaz <hamedefaz@gmail.com>
2022-08-09 02:10:13 +0600

    Update Dashboard
    Edited dashboard to manage user list and view data from the database.

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,16 @@ Route::get('/', 'Frontend\FrontendController@index');
 Route::get('about-us','Frontend\FrontendController@aboutUs')->name('about.us');
 Route::get('contact-us','Frontend\FrontendController@contactUs')->name('contact.us');
 
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::prefix('users')->group(function(){
+    Route::get('/view', 'Backend\UserController@view')->name('user.view');
+    Route::get('/add', 'Backend\UserController@add')->name('user.add');
+    Route::post('/store', 'Backend\UserController@store')->name('user.store');
+    Route::get('/edit/{id}', 'Backend\UserController@edit')->name('user.edit');
+    Route::post('/update/{id}', 'Backend\UserController@update')->name('user.update');
+    Route::get('/delete/{id}', 'Backend\UserController@delete')->name('user.delete');
+});
