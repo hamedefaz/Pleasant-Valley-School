2a6e2baf9c8538cc2a7056642f75d69ef89a1424
hamedefaz <hamedefaz@gmail.com>
2022-08-15 13:05:12 +0600

    Active Menu and Middleware
    Implemented active menu view on dashboard sidebar and middleware for redirecting URLs.

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,9 +1,13 @@
-      <!-- Sidebar Menu -->
+@php
+  $prefix = Request::route()->getPrefix();
+  $route = Route::current()->getName();
+@endphp
+     <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           @if (Auth::user()->usertype=='Admin')
 
-          <li class="nav-item">
+          <li class="nav-item {{($prefix=='/users')?'menu-open':''}}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -14,7 +18,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('users.view')}}" class="nav-link">
+                <a href="{{route('users.view')}}" class="nav-link {{($route=='users.view')?'active':''}}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>View User</p>
                 </a>
@@ -23,7 +27,7 @@
             </ul>
           </li>
           @endif
-          <li class="nav-item">
+          <li class="nav-item {{($prefix=='/profiles')?'menu-open':''}}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -34,13 +38,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('profiles.view')}}" class="nav-link">
+                <a href="{{route('profiles.view')}}" class="nav-link {{($route=='profiles.view')?'active':''}}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Your Profile</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('profiles.password.view')}}" class="nav-link">
+                <a href="{{route('profiles.password.view')}}" class="nav-link {{($route=='profiles.password.view')?'active':''}}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Change Password</p>
                 </a>
